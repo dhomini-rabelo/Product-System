9ea3d6d32edeb68fd8ba47a7bc3c4f4746e2b54a
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-20 15:33:41 -0300

    add models to admin

--- backend/project/backend/products/app/admin.py
+++ backend/project/backend/products/app/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
+from backend.products.app.models import Category, PriceMediator, Product
+
 
 admin.site.empty_value_display = 'NULL'
 
 
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'name',
+    list_per_page = 50
+    list_select_related = 'category', # use tuple, default is False
+    ordering = 'name',
+    search_fields = 'name', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'name',
+    list_per_page = 50
+    ordering = 'name',
+    search_fields = 'name', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
+
+
+@admin.register(PriceMediator)
+class PriceMediatorAdmin(admin.ModelAdmin):
+    list_display = 'id', 'price',
+    list_display_links = 'price',
+    list_per_page = 50
+    list_select_related = 'provider', 'product'
+    ordering = 'price',
+    search_fields = 'price',
--- backend/project/backend/providers/app/admin.py
+++ backend/project/backend/providers/app/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
+from backend.providers.app.models import Contact, Provider, Address
+
 
 admin.site.empty_value_display = 'NULL'
 
 
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name',
+    list_display_links = 'name',
+    list_per_page = 50
+    list_select_related = 'address', # use tuple, default is False
+    ordering = 'name',
+    search_fields = 'name', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
+
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'number',
+    list_display_links = 'number',
+    list_per_page = 50
+    list_select_related = 'provider', # use tuple, default is False
+    ordering = 'number',
+    search_fields = 'number', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = 'id', 'city',
+    list_display_links = 'city',
+    list_per_page = 50
+    ordering = 'city',
+    search_fields = 'city', # ^ -> startswith, = -> iexact, @ ->	search, None -> icontains
