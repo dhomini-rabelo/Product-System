1eb528eea0280e149ea47f58327852ce5513e24e
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-20 12:49:42 -0300

    adding project models

--- backend/project/backend/products/app/models.py
+++ backend/project/backend/products/app/models.py
@@ -1,1 +1,25 @@
+from ast import For
 from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField, SET_NULL, CASCADE)
+from backend.providers.app.models import Provider
+
+
+
+
+class Category(Model):
+    name = CharField(max_length=200, unique=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+
+class Product(Model):
+    name = CharField(max_length=200, unique=True)
+    description = TextField()
+    category = ForeignKey(Category, on_delete=SET_NULL, null=True, related_name='products')
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+
+class PriceMediator(Model):
+    product = ForeignKey(Product, on_delete=CASCADE, related_name='providers')
+    provider = ForeignKey(Provider, on_delete=CASCADE, related_name='products')
+    price = DecimalField(decimal_places=2, max_digits=20)
--- backend/project/backend/providers/app/models.py
+++ backend/project/backend/providers/app/models.py
@@ -1,1 +1,18 @@
 from django.db.models import (Model, CharField, DateTimeField, TextField, EmailField, ForeignKey, PositiveIntegerField, ImageField, RESTRICT, DecimalField, DateField, BooleanField, SET_NULL, CASCADE)
+
+
+class Address(Model):
+    city = CharField(max_length=256)
+    country = CharField(max_length=256)
+
+
+class Provider(Model):
+    name = CharField(max_length=200, unique=True)
+    social_role = TextField()
+    cnpj = CharField(max_length=18)
+    address = ForeignKey(Address, on_delete=SET_NULL, null=True)
+
+
+class Contact(Model):
+    provider = ForeignKey(Provider, on_delete=CASCADE, related_name='contacts')
+    number = CharField(max_length=30)
