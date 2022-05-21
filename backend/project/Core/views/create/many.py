a9bf8a8433497b31a23883e89e79628df61632cb
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-21 16:25:05 -0300

    update support serializer

--- backend/project/Core/views/create/many.py
+++ backend/project/Core/views/create/many.py
@@ -3,8 +3,17 @@ from django.db.models.query import QuerySet
 from django.db.models import Model
 
 
-class CreatorForSerializerWithManyFields:
+class SerializerSupport:
 
+    def get_or_error(
+        self, queryset: QuerySet, pk_kwargs: dict = {}, error_obj = {}
+    ) -> Model: # for foreign key
+        relationship_instance = queryset.filter(**pk_kwargs).first()
+
+        if relationship_instance is None:
+            raise ValidationError(error_obj)
+        return relationship_instance
+    
     def get_or_create_instance(
         self, data: dict, serializer_class: ModelSerializer, queryset: QuerySet,
         pk_kwargs: dict = {}, instance_id: int | None = None, related_name: str = '',
@@ -14,6 +23,33 @@ class CreatorForSerializerWithManyFields:
         if relationship_instance is None:
             return self.create_instance(data, serializer_class, instance_id, related_name)
         return relationship_instance
+
+    def update_instance(
+        self, scope_instance: Model, data_for_update: dict,
+    ) -> Model: # for o2o and foreign key
+        for attribute_name, value in data_for_update.items():
+            if value is None: continue
+            setattr(scope_instance, attribute_name, value)
+        scope_instance.save()
+        return scope_instance        
+
+    def create_or_update_instance(
+        self, data: dict, serializer_class: ModelSerializer, queryset: QuerySet,
+        pk_kwargs: dict = {}, instance_id: int | None = None, related_name: str = '',
+    ) -> Model: # for foreign key
+        relationship_instance = queryset.filter(**pk_kwargs).first()
+
+        if relationship_instance is None:
+            return self.create_instance(data, serializer_class, instance_id, related_name)
+        return self.update_instance(relationship_instance, data)
+
+    def create_or_update_many(
+        self, data: list[dict], serializer_class: ModelSerializer, queryset: QuerySet,
+        pk_kwargs: dict = {},  instance_id: Model | None = None, related_name: str = ''
+    ): # m2m or m2o
+        for obj in data:
+            adapted_kwargs = {k: obj.get(v)  for k, v in pk_kwargs.items()}
+            self.create_or_update_instance(obj, serializer_class, queryset, adapted_kwargs, instance_id, related_name)        
 
     def create_instance(
         self, data: dict, serializer_class: ModelSerializer,
@@ -26,10 +62,27 @@ class CreatorForSerializerWithManyFields:
             relationship_instance = serializer.save()
             return relationship_instance
         raise ValidationError(serializer.errors)
-        
+
     def create_many(
         self, data: list[dict], serializer_class: ModelSerializer,
         instance_id: int | None = None, related_name: str = ''
     ): # m2m or m2o
         for obj in data:
             self.create_instance(obj, serializer_class, instance_id, related_name)
+
+    def error_or_update_instance(
+        self, data: dict, queryset: QuerySet, pk_kwargs: dict = {}, error_obj = {}
+    ) -> Model: # for foreign key
+        relationship_instance = queryset.filter(**pk_kwargs).first()
+
+        if relationship_instance is None:
+            raise ValidationError(error_obj)
+        return self.update_instance(relationship_instance, data)
+    
+    def error_or_update_many(
+        self, data: list[dict], queryset: QuerySet, pk_kwargs: dict = {}, error_obj = {}
+    ) -> Model: # for foreign key
+        for obj in data:
+            adapted_kwargs = {k: obj.get(v)  for k, v in pk_kwargs.items()}
+            print(adapted_kwargs)
+            self.error_or_update_instance(obj, queryset, adapted_kwargs, error_obj)
