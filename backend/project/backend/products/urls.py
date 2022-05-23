a389d4cee55f1c3dacf598ff39235e52474e513d
dhomini-rabelo <dhominisalgado@gmail.com>
2022-05-23 14:24:06 -0300

    remove route bug from bar

--- backend/project/backend/products/urls.py
+++ backend/project/backend/products/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 from .views import *
 
+
 app_name = 'products'
 
+# duplicate to remove route bug from bar
 urlpatterns = [
+    path('products/', ProductDataControlAndCreate.as_view()),
     path('products', ProductDataControlAndCreate.as_view(), name='product_list'),
+
+    path('products/<int:pk>/', ProductDetail.as_view()),
     path('products/<int:pk>', ProductDetail.as_view(), name='product_detail'),
+
+    path('categories/', CategoryListAndCreate.as_view()),
     path('categories', CategoryListAndCreate.as_view(), name='category_list'),
+
+    path('categories/<int:pk>/', CategoryDetail.as_view()),
     path('categories/<int:pk>', CategoryDetail.as_view(), name='category_detail'),
 ]
--- backend/project/backend/providers/urls.py
+++ backend/project/backend/providers/urls.py
@@ -4,8 +4,11 @@ from .views import *
 
 app_name = 'providers'
 
-
+# duplicate to remove route bug from bar
 urlpatterns = [
+    path('providers/', ProviderDataControlAndCreate.as_view()),
     path('providers', ProviderDataControlAndCreate.as_view(), name='provider_list'),
+
+    path('providers/<int:pk>/', ProviderDetail.as_view()),
     path('providers/<int:pk>', ProviderDetail.as_view(), name='provider_detail'),
 ]
